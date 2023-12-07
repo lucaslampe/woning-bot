c6bab482157a934b4a534f569549c86ac9370bd0
Lucas Lampe <lucaslampe@gmail.com>
2023-12-07 11:14:46 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,8 +16,7 @@ const houses = [];
 const { CHAT_ID, BOT_API } = process.env;
 
 const urls = [
-    'https://www.funda.nl/en/koop/amsterdam/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
-    'https://www.funda.nl/en/koop/haarlem/beschikbaar/0-300000/40+woonopp/2+slaapkamers/1-dag/',
+    'https://www.funda.nl/en/huur/utrecht/beschikbaar/0-1750/40+woonopp/2+slaapkamers/1-dag/',
 ];
 
 const runTask = async () => {
